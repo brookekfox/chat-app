2feb76cb02a7c8a77c1ac8629d322ef9ebbda1c7
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-04-17 16:52:07 -0700

    chat app with facebook integration, angular 2 and firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,13 @@
+import { AngularFireModule } from 'angularfire2';
+export const firebaseConfig = {
+  apiKey: "AIzaSyA1qOx6Y-rocwj_H9v-xTEhLGxJzj8Jy9o",
+  authDomain: "mychatapp-51fe8.firebaseapp.com",
+  databaseURL: "https://mychatapp-51fe8.firebaseio.com",
+  projectId: "mychatapp-51fe8",
+  storageBucket: "mychatapp-51fe8.appspot.com",
+  messagingSenderId: "118597090918"
+};
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
